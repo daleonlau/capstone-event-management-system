537eee5cb5e87c529726b83b4353b45898d304d6
daleonlau <laurencedaleon64@gmail.com>
2026-04-13 23:19:49 +0800

    basta ma fix lang ning auto generated reports

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'EventFlow') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/csu-logo.png') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
     @vite('resources/js/app.js')
